f4d7bebbede3b68790417b3e6f991c855103471c
slicebo123 <slicebo123@gmail.com>
2014-07-14 23:25:48 -0700

    Adds some Mailer tests

--- spec/mailers/mailer_spec.rb
+++ spec/mailers/mailer_spec.rb
@@ -1,5 +1,24 @@
 require "spec_helper"
 
 describe Mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#forward' do
+    let(:mail_options) { {to: "you@yours.com", subject: "foobar", body: "I like ruby"} }
+    let(:mail) { Mailer.forward(to: mail_options[:to], subject: mail_options[:subject], body: mail_options[:body]) }
+
+    it 'should have the correct subject' do
+      expect(mail.subject).to eq(mail_options[:subject])
+    end
+
+    it 'should have the correct recipient' do
+      expect(mail.to).to eq([mail_options[:to]])
+    end
+
+    it 'should have the correct sender' do
+      expect(mail.from).to eq(["no-reply@example.com"])
+    end
+
+    it 'should have correct body content' do
+      expect(mail.body.encoded).to include("ruby")
+    end
+  end
 end
