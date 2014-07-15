fdfc5671a95d6cc3d6fab6dcf61653092afc139a
slicebo123 <slicebo123@gmail.com>
2014-07-14 19:22:00 -0700

    Initial Commit. Rails-new an application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+APIMailer::Application.config.secret_key_base = '502624e2c616ba86bfd9833ad661de73243a9daa6920deed2d14a9b5aea111e544bf5e26a77967edc329d733a08497d933314a2cc2c2d49b341cb69e28ed5ebc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+APIMailer::Application.config.session_store :cookie_store, key: '_API_Mailer_session'
