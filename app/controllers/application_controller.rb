3e274056170b19d5a9e263e02952b75ec59a782e
slicebo123 <slicebo123@gmail.com>
2014-07-15 00:43:10 -0700

    Make root return 200 so New Relic pinger wont send alerts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,10 @@ class ApplicationController < ActionController::Base
     render text: e.message, status: :unprocessable_entity
   end
 
+  def home
+    render nothing: true, status: :ok
+  end
+
   def not_found
     render nothing: true, status: :not_found
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 APIMailer::Application.routes.draw do
+  root to: 'application#home'
   resource :mail, only: [:create], controller: :mail
   match "*path", to: "application#not_found", via: :all
 end
